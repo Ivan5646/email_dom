d0d9a49ca0e8c95bf1c57fa8995f7c0d6ad3d264
Ivan5646 <Mike000027@yandex.ru>
2017-07-16 21:27:33 +0700

    first

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+var gulp = require('gulp');
+var uglifycss = require('gulp-uglifycss'); // to minify files
+var concat = require('gulp-concat');
+var minify = require('gulp-minify'); // minify js
+var watch = require('gulp-watch');
+var connect = require('gulp-connect');
+var less = require('gulp-less');
+// var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('connect', function() { // ok
+  connect.server({
+    root: 'build',
+    livereload: true
+  });
+});
+
+gulp.task('app-styles', function() { // ok
+  return gulp.src("src/styles/**/*.less")
+    .pipe(less())
+    // .pipe(autoprefixer({
+    //   browsers: ['last 2 versions'],
+    //   cascade: false
+    // }))
+    .pipe(uglifycss())
+    .pipe(gulp.dest("build/styles"))
+    .pipe(connect.reload());
+});
+
+// gulp.task('app-scripts', function() { // ok
+//   return gulp.src("src/js/*.js")
+//     .pipe(concat("all.js"))
+//     .pipe(minify())
+//     .pipe(gulp.dest("build/js"))
+//     .pipe(connect.reload());
+// });
+
+gulp.task('watch', function () { // ok
+  var appStyles = ['app-styles'];
+  var appScripts = ['app-scripts'];
+  // var index = ['index'];
+
+  gulp.watch('src/styles/**/*.less', appStyles);
+  gulp.watch('src/js/*.js', appScripts);
+});
+
+gulp.task('default', ['connect', 'watch']);
